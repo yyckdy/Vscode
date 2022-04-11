504c916f1f0e9b47997d0acaa747e38928fdd64d
yyckdy <48600672@qq.com>
2022-04-11 16:21:36 +0800

    第一次修改

--- STL/ 常用排序算法_random_shuffle.cpp
+++ STL/ 常用排序算法_random_shuffle.cpp
@@ -1,38 +1,37 @@
-//#include<iostream>
-//using namespace std;
-//#include <vector>
-//#include <algorithm>
-//#include <ctime>
-//
-////³£ÓÃÅÅĞòËã·¨  random_shuffle
-//
-//void myPrint(int val)
-//{
-//	cout << val << " ";
-//}
-//
-//void test01()
-//{
-//	srand((unsigned int)time(NULL));
-//
-//	vector<int>v;
-//	for (int i = 0; i < 10; i++)
-//	{
-//		v.push_back(i);
-//	}
-//
-//	//ÀûÓÃÏ´ÅÆ Ëã·¨ ´òÂÒË³Ğò
-//	random_shuffle(v.begin(), v.end());
-//
-//	for_each(v.begin(), v.end(), myPrint);
-//	cout << endl;
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+#include <ctime>
+
+//å¸¸ç”¨æ’åºç®—æ³•  random_shuffle
+
+void myPrint(int val)
+{
+	cout << val << " ";
+}
+
+void test01()
+{
+	srand((unsigned int)time(NULL));
+
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+
+	//åˆ©ç”¨æ´—ç‰Œ ç®—æ³• æ‰“ä¹±é¡ºåº
+	// random_shuffle(v.begin(), v.end());
+
+	for_each(v.begin(), v.end(), myPrint);
+	cout << endl;
+}
+
+int main() {
+
+	test01();
+
+
+	return 0;
+}
